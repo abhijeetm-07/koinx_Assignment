7420c9599831fbae1c1a5dc09c7ce215697b0f46
Abhijeet.M <abhijeetiyer07@gmail.com>
2026-04-17 08:09:02 +0530

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white text-3xl font-bold">
+      Tailwind is working
+    </div>
+  );
+}
+
+export default App;
